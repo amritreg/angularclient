fc678e69affe4ab5cca14eb5124295f4146d6c4a
Amrit <amritregmi@Amrits-MacBook-Pro.local>
2019-04-09 09:41:58 +1000

    first commit

---
+++ src/app/customer.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  private baseUrl = 'http://localhost:8000/customers';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCustomer(id: number): Observable<object
+
+  width = "300"
+  height = "150" > {
+    return this.http.get(`${this.baseUrl}/${id}`);
+}
+
+createCustomer(customer
+:
+Object
+):
+Observable < object > {
+  return this.http.post(`${this.baseUrl}/`, customer);
+}
+
+updateCustomer(id
+:
+number, value
+:
+any
+):
+Observable < object > {
+  return this.http.put(`${this.baseUrl}/${id}`, value);
+}
+
+deleteCustomer(id
+:
+number
+):
+Observable < any > {
+  return this.http.delete(`${this.baseUrl}/${id}`);
+}
+
+getCustomersList()
+:
+Observable < any > {
+  return this.http.get(`${this.baseUrl}/`);
+}
+
+getCustomersByAge(age
+:
+number
+):
+Observable < any > {
+  return this.http.get(`${this.baseUrl}/age/${age}/`);
+}
+
+deleteAll()
+:
+Observable < any > {
+  return this.http.delete(`${this.baseUrl}/`);
+}
+}
